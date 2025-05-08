580d99df0f5e160f1623aecea5d7ddae625fe384
Madokakaroto <180185184@qq.com>
2025-05-08 10:24:34 +0800

    add boost/pfr, boost/preprocessor, asio standalone

--- include/Utils/StaticReflection.hpp
+++ include/Utils/StaticReflection.hpp
@@ -1,1 +1,2 @@
 #pragma once
+#include "boost/pfr.hpp"
--- main.cpp
+++ main.cpp
@@ -2,29 +2,40 @@
 #include "async_simple/coro/Lazy.h"
 #include "async_simple/coro/SyncAwait.h"
 
+#include "Utils/StaticReflection.hpp"
+
 struct foo {};
 class fee {};
 
+
+struct bar
+{
+    float float_value;
+};
+
 async_simple::coro::Lazy<int> get_43() {
-	std::cout << "run with ::operator new/delete" << '\n';
-	co_return 43;
+    std::cout << "run with ::operator new/delete" << '\n';
+    co_return 43;
 }
 
 int main(void)
 {
-	constexpr ecs::entity_t e{};
-	static_assert(!e.is_valid());
+    constexpr ecs::entity_t e{};
+    static_assert(!e.is_valid());
 
-	std::cout << "hello ecs" << std::endl;
+    std::cout << "hello ecs" << std::endl;
 
-	static_assert(ecs::murmur_hash_x86_32("hello ecs", 0x453627) != 0);
+    static_assert(ecs::murmur_hash_x86_32("hello ecs", 0x453627) != 0);
 
-	std::cout << ecs::get_demangle_name<int>() << std::endl;
-	std::cout << ecs::get_demangle_name<std::string>() << std::endl;
-	std::cout << ecs::get_demangle_name<foo>() << std::endl;
-	std::cout << ecs::get_demangle_name<fee>() << std::endl;
+    std::cout << ecs::get_demangle_name<int>() << std::endl;
+    std::cout << ecs::get_demangle_name<std::string>() << std::endl;
+    std::cout << ecs::get_demangle_name<foo>() << std::endl;
+    std::cout << ecs::get_demangle_name<fee>() << std::endl;
 
-	syncAwait(get_43());
+    syncAwait(get_43());
 
-	return 0;
+
+    static_assert(boost::pfr::tuple_size_v<bar> == 1);
+
+    return 0;
 }
